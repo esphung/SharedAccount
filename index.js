fd6f621830cbaad80249fea81ec0c70d23954d6c
Eric Phung <esphung@gmail.com>
2025-03-18 17:06:46 -0500

    feat: added storybook

--- index.js
+++ index.js
@@ -3,7 +3,14 @@
  */
 
 import { AppRegistry } from "react-native";
-import App from "./App";
+import { default as Storybook } from "./.storybook";
 import { name as appName } from "./app.json";
+import { showStorybook } from "./package.json";
+import App from "./src/App";
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => {
+  if (showStorybook) {
+    return Storybook;
+  }
+  return App;
+});
